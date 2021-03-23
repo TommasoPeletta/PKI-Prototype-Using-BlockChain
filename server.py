f880b0c739ac5c3cf80574b53ef04b1b42368015
TommasoPeletta <tommaso.peletta@etu.unige.ch>
2021-03-23 11:42:49 +0100

    add new key and update key base

--- server.py
+++ server.py
@@ -9,32 +9,49 @@ allClient = []
 #Each instance has the socket and address that is associated with items
 #Along with an assigned ID and a name chosen by the client
 
-def VerifyPk(pk, Client):
-    return addAllClient(pk, Client)
+def VerifyPk(pk, mac, sign, Client):
+    #consensus group
+    return "verified"
     #time.sleep(10)
 
-def addAllClient(pk, Client):
+def addAllClient(pk, mac, Client):
     for i in allClient:
-        if i[0] == pk:
+        if i[0] == pk and i[1] == mac:
             i[1] = Client
             i[2] = 1
-            return "not first time"
-    allClient.append([pk, Client, 1])
-    Client.pk = pk
-    return "first time"
+            Client.signal = 1
+            return "successfully connected"
+        elif i[0] == pk and i[1] != mac:
+            return "pk already in use"
+            #pk already in use error
+        elif i[0] != pk and i[1] == mac:
+            Client.signal = 2
+            return "sign your last message if you want to update your pk"
+    print("yess")
+    Client.signal = 3
+    return "sign your last message if you want to save your pk"
+'''
+            Client.pk = pk
+            Client.mac = mac
+            allClient.append([pk, mac, Client, 1])
+'''
+
 
 def discClient(Client):
     for i in allClient:
-        if i[0] == Client.pk:
+        if i[1] == Client.mac and i[0] == Client.pk:
             i[2] = 0
+            Client.signal = 0
 
 class Client(threading.Thread):
-    def __init__(self, socket, address, pk, signal):
+    def __init__(self, socket, address, pk, mac, signal,c):
         threading.Thread.__init__(self)
         self.socket = socket
         self.address = address
         self.pk = pk
+        self.mac = mac
         self.signal = signal
+        self.c = c
 
     def __str__(self):
         return str(self.pk) + " " + str(self.address)
@@ -47,17 +64,31 @@ class Client(threading.Thread):
 
     def run(self):
         self.socket.sendall(str.encode("waiting for signed pk"))
-        while self.signal:
+        while self.c:
             try:
                 data = self.socket.recv(32)
                 parsed = data.split()
                 if len(parsed) > 1:
-                    if parsed[0] == b'pk':
-                        ver = VerifyPk(parsed[1], self)
-                        self.socket.sendall(str.encode(ver))
+                    print(self.signal)
+                    if self.signal == 0 or self.signal == 1: # client want to connect
+                        if parsed[0] == b'pk' and parsed[2] == b'mac':
+                            con = addAllClient(parsed[1],parsed[3], self)
+                            self.socket.sendall(str.encode(con))
+                    if self.signal == 2 and len(parsed) > 5: # client want to update pk
+                        if parsed[0] == b'pk' and parsed[2] == b'mac':
+                            if parsed[4] == b'sign':
+                                ver = VerifyPk(parsed[1], parsed[3], parsed[5],self)
+                                self.socket.sendall(str.encode(ver))
+                    if self.signal == 3 and len(parsed) > 5: # client want to save new pk
+                        if parsed[0] == b'pk' and parsed[2] == b'mac':
+                            if parsed[4] == b'sign':
+                                ver = VerifyPk(parsed[1], parsed[3], parsed[5],self)
+                                self.socket.sendall(str.encode(ver))
+
             except Exception as e:
                 print("Client " + str(self.address) + " has disconnected")
-                self.signal = False
+                self.socket.sendall(str.encode("You have been disconnected"))
+                self.c = False
                 discClient(self)
                 connections.remove(self)
                 print(e)
@@ -68,7 +99,7 @@ def newConnections(socket):
     while True:
         sock, address = socket.accept()
         global total_connections
-        connections.append(Client(sock, address, 0, True))
+        connections.append(Client(sock, address, 0, 0, 0, True))
         connections[len(connections) - 1].start()
         print("New connection at ID " + str(connections[len(connections) - 1]))
         total_connections += 1
@@ -81,6 +112,7 @@ def listenServer(newConnectionsThread):
             print(connections[0].socket)
             print(connections[0].address)
             print(connections[0].pk)
+            print(connections[0].mac)
             print(connections[0].signal)
 
 
